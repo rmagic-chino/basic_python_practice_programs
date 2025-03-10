0dbd2008c3f1c24df04ed14bfb738e83234a4192
Carlos De Guzman <raremagic01@gmail.com>
2025-03-11 00:08:46 +0800

    enter two numbers and determine

--- batch_1_programs/greater_lesser_equal.py
+++ batch_1_programs/greater_lesser_equal.py
@@ -1,5 +1,13 @@
 #input two numbers
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
 #check if the first number is greater than the second number
+if num1 > num2:
+    print("The first number is greater than the second number.")
 #check if the first number is less than the second number
+elif num1 < num2:
+    print("The first number is less than the second number.")
 #check if the first number is equal to the second number
+else:
+    print("The first number is equal to the second number.")
     #display the result
