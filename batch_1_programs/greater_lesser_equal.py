f44489263277de952999a492486296fa56855e06
Carlos De Guzman <raremagic01@gmail.com>
2025-03-11 00:03:47 +0800

    Add pseudocode for greater, lesser, and equal comparisons

---
+++ batch_1_programs/greater_lesser_equal.py
@@ -0,0 +1,5 @@
+#input two numbers
+#check if the first number is greater than the second number
+#check if the first number is less than the second number
+#check if the first number is equal to the second number
+    #display the result
