3014dbc7317b1179f76439eb1107a5382623c02b
Carlos De Guzman <raremagic01@gmail.com>
2025-03-11 00:24:27 +0800

    assign conditions and determine

--- batch_1_programs/equal_!equal.py
+++ batch_1_programs/equal_!equal.py
@@ -1,4 +1,12 @@
 #input two numbers
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
 #set condition for the first number to be greater than the second number
+if num1 == num2:
+    print("The two numbers are equal")
 #set condition for the first number to be less than the second number
+elif num1 != num2:
+    print("The two numbers are not equal")
      #display the result
+else:
+    print("The two numbers are not equal")
