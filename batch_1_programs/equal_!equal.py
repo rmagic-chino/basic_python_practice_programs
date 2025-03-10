7e5aa80ac2feb66f97a9272887360d7eb59b326f
Carlos De Guzman <raremagic01@gmail.com>
2025-03-11 00:21:46 +0800

    pseudocode

---
+++ batch_1_programs/equal_!equal.py
@@ -0,0 +1,4 @@
+#input two numbers
+#set condition for the first number to be greater than the second number
+#set condition for the first number to be less than the second number
+     #display the result
