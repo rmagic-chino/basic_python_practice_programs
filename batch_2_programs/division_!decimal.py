4941a80f6538d9c4e81ee4ce51afb5304991f523
Carlos De Guzman <raremagic01@gmail.com>
2025-03-12 01:23:51 +0800

    division no decimal

--- batch_2_programs/division_!decimal.py
+++ batch_2_programs/division_!decimal.py
@@ -1,6 +1,13 @@
 #input two numbers
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
 
 #set rules
+if num2 != 0:
+    quotient = int(num1 // num2)
+    print("The quotient is", quotient)
    #disable deciamls
 #output division
+else:
+    print("Division by zero is not allowed")
    #deny division by zero
