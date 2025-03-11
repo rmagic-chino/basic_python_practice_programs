8e1ef7966dd62f7e087c0f5e38963c2c00b34c4f
Carlos De Guzman <raremagic01@gmail.com>
2025-03-12 01:16:17 +0800

    subract two numbers

--- batch_2_programs/subtraction.py
+++ batch_2_programs/subtraction.py
@@ -1,4 +1,8 @@
 #input two numbers
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
 
 #set rules
+difference = num1 - num2
+print("The difference is", difference)
 #print difference
