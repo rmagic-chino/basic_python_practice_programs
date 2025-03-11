981b81c66b32d6b2db4d33dc03bfe5a3c93688e0
Carlos De Guzman <raremagic01@gmail.com>
2025-03-12 03:17:23 +0800

    unique numbers only

--- batch_3_programs/unique_numbers.py
+++ batch_3_programs/unique_numbers.py
@@ -1,9 +1,19 @@
 #initialize
+values = []
 
 #initialize for loop
+for number in range(10):
+    num = float(input("Enter a number: "))
+    values.append(num)
 #input 10 numbers
 
 #initiaize a list to store the unique numbers
+unique = []
+
 #initialize a for loop to iterate through the list of numbers
+for num in values:
+    if values.count(num) == 1:
+        unique.append(num)
 
 #print the unique numbers
+print("Unique numbers: ", unique)
