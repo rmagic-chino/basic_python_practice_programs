f525557fc16a467152d3e107f11c449692d0a771
Carlos De Guzman <raremagic01@gmail.com>
2025-03-12 02:30:49 +0800

    subtract 10 numbers

--- batch_2_programs/minus_10num.py
+++ batch_2_programs/minus_10num.py
@@ -1,7 +1,16 @@
 #initialize
+numbers = []
 
 #input 10 numbers
+for number in range(10):
+    num = float(input("Enter a number: "))
+    numbers.append(num)
 
 #set rules and coditions
+result = numbers[0]
+
 #subtract 10 from each number
+for num in numbers[1:]:
+    result -= num
 #output
+print(result)
