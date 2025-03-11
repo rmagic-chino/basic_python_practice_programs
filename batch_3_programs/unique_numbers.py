6e24ac05a737530acaaf08027daaa4243e397d0a
Carlos De Guzman <raremagic01@gmail.com>
2025-03-12 03:13:59 +0800

    pseudocode

---
+++ batch_3_programs/unique_numbers.py
@@ -0,0 +1,9 @@
+#initialize
+
+#initialize for loop
+#input 10 numbers
+
+#initiaize a list to store the unique numbers
+#initialize a for loop to iterate through the list of numbers
+
+#print the unique numbers
