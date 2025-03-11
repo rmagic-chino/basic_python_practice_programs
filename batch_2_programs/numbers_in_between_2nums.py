8c327b26cf129d14772c54e926d9017d6215394f
Carlos De Guzman <raremagic01@gmail.com>
2025-03-12 02:48:30 +0800

    print numbers in between 2 numbers

--- batch_2_programs/numbers_in_between_2nums.py
+++ batch_2_programs/numbers_in_between_2nums.py
@@ -1,6 +1,11 @@
 #input 2 numbers
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
 
 #initialize
-
+if num1 > num2
+    num1, num2 = num2, num1
 #for loop to print numbers between 2 numbers
+for num in range(num1 + 1, num2):
+    print(num)
    #print output
