2a1663a5d96722ee2c8867de4828adecb0c62d4a
Carlos De Guzman <raremagic01@gmail.com>
2025-03-12 00:11:31 +0800

    raised to exponent

--- batch_1_programs/num1_exponent.py
+++ batch_1_programs/num1_exponent.py
@@ -1,4 +1,8 @@
 #input two numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
 
 #set rules
+result = num1 ** num2
 #print answer
+print("The ansnwer is: ", result)
