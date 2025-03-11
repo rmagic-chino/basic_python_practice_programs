1878bad00049e4a6839fbcb84af2bc57be803b91
Carlos De Guzman <raremagic01@gmail.com>
2025-03-12 01:28:49 +0800

    print the remainder of two numbers

--- batch_2_programs/print _remainder.py
+++ batch_2_programs/print _remainder.py
@@ -1,5 +1,12 @@
 #input two numbers
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
 
 #set rules and coditions
+if num2 != 0:
+    remainder = num1 % num2
+    print("remainder is: ", remainder)
     #print output
+else:
+    print("Division by zero is not allowed")
 #deny division by zero
