8d5a29d0ed7cdffdec29860d18f5244b056258b5
Carlos De Guzman <raremagic01@gmail.com>
2025-03-12 01:12:30 +0800

    equal_or_not

--- !equal_equal.py
+++ !equal_equal.py
@@ -1,4 +1,11 @@
 #input two numbers
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
 
 #set conditions
+if num1 != num2:
+    print("not equal")
 #print output
+else:
+    print("equal")
+#end
