4398f139989dbef8dce09d7b4b6e3afd4898f044
Carlos De Guzman <raremagic01@gmail.com>
2025-03-12 01:08:28 +0800

    print smaller

--- batch_2_programs/smaller_number.py
+++ batch_2_programs/smaller_number.py
@@ -1,4 +1,8 @@
 #input two numbers
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
 
 #set rules
+smaller = min(num1, num2)
+print("The smaller number is", smaller)
 #print smaller number
