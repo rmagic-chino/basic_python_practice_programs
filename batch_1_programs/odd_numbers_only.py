918cea0bfa3da1b0fa4c839830cfaf267fa21f24
Carlos De Guzman <raremagic01@gmail.com>
2025-03-12 00:28:11 +0800

    print ony odd

--- batch_1_programs/odd_numbers_only.py
+++ batch_1_programs/odd_numbers_only.py
@@ -1,7 +1,15 @@
 #initialize
+count = 0
 
 #display message enter 10 numbers
+print("Enter 10 numbers:")
+for number in range(10):
+    num = int(input())
+    if num % 2 != 0:
+        print(num)
+        count += 1
 #put conditions and display only odd numbers
      #put rules for odd numbers
 
+print("Number of odd numbers:", count)
 #print the odd numbers
