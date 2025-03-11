4000446b7ba63e683798149fbd2a513d781861e5
Carlos De Guzman <raremagic01@gmail.com>
2025-03-12 02:41:40 +0800

    1-100 !ending 0 and 5

--- batch_2_programs/0-100_!ending0_5.py
+++ batch_2_programs/0-100_!ending0_5.py
@@ -1,6 +1,11 @@
 #initialize
+num = 0
 
 #while loop to print numbers 0-100
+while num <= 100:
+    if num % 10 != 0 and num % 5 != 0:
+        print(num)
     #set rules and conditions
+    num += 1
        #print numbers 0-100
     #print output
