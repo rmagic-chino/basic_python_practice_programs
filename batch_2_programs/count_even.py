e65340902e460ad576c79f9ca67c42b33fe2d719
Carlos De Guzman <raremagic01@gmail.com>
2025-03-12 02:34:50 +0800

    even numbers count

--- batch_2_programs/count_even.py
+++ batch_2_programs/count_even.py
@@ -1,6 +1,12 @@
 #initalize
+even_count = 0
 
 #set rules and conditions
+for number in range(10):
+    num = int(input("Enter a number: "))
+    if number % 2 == 0:
+        even_count += 1
 #set variables
 
+print("The number of even numbers is: ", even_count)
 #print output
