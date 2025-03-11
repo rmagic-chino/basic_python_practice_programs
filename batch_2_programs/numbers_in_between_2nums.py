1bd0469b4535501df30bb7fa8ce7c3222159681e
Carlos De Guzman <raremagic01@gmail.com>
2025-03-12 02:49:00 +0800

    updated numbers_in_between_2nums.py

--- batch_2_programs/numbers_in_between_2nums.py
+++ batch_2_programs/numbers_in_between_2nums.py
@@ -3,7 +3,7 @@ num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 
 #initialize
-if num1 > num2
+if num1 > num2:
     num1, num2 = num2, num1
 #for loop to print numbers between 2 numbers
 for num in range(num1 + 1, num2):
