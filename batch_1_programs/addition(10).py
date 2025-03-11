4db205cc11041034f23605aba1e0424f1754b1f3
Carlos De Guzman <raremagic01@gmail.com>
2025-03-12 00:18:59 +0800

    add 10 numbers

--- batch_1_programs/addition(10).py
+++ batch_1_programs/addition(10).py
@@ -1,6 +1,12 @@
 #initialize empty list
+numbers = []
 
 #input 10 numbers
+for number in range(10):
+    number = int(input("Enter a number: "))
+    numbers.append(number)
 
 #set conditions
+total = sum(numbers)
 #print result
+print("The sum is:", total)
