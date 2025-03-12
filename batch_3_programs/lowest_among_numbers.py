c5c33a9eb58cc075944bc1cfc59ebbc990dd76a1
Carlos De Guzman <raremagic01@gmail.com>
2025-03-12 10:59:30 +0800

    print smallest number among numbers

--- batch_3_programs/lowest_among_numbers.py
+++ batch_3_programs/lowest_among_numbers.py
@@ -1,8 +1,15 @@
 #initialize
+lowest = None
 
 #while loop
     #set rules for the loop
-    #print the lowest number
-#end loop
+while True:
+    try:
+        num = float(input("Enter a number: "))
+        if lowest is None or num < lowest:
+            lowest = num
+    except ValueError:
+        break
 
+print("The lowest number is", lowest) #
 #print the lowest number
