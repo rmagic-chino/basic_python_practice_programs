0c4e7e6b240dcd770542efe8b712d5e0ab5f2d7e
Carlos De Guzman <raremagic01@gmail.com>
2025-03-12 10:12:23 +0800

    print nums with duplicates

--- batch_3_programs/duplicates.py
+++ batch_3_programs/duplicates.py
@@ -1,5 +1,13 @@
 #initialize
+values = []
+unique = []
 
 #initialize rules and condiitons
+for number in range(10):
+    num = float(input("Enter a number: "))
+    if num not in unique:
+        unique.append(num)
+    values.append(num)
 
 #print output
+print("Unique numbers: ", unique)
