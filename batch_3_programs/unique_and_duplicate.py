3f2eb833b5c72ed82b6397a1e786b87433339921
Carlos De Guzman <raremagic01@gmail.com>
2025-03-12 10:36:38 +0800

    determine unique and duplicate

--- batch_3_programs/unique_and_duplicate.py
+++ batch_3_programs/unique_and_duplicate.py
@@ -1,8 +1,19 @@
 #initialize
+values = []
 
 #while loop enter as many numbers
-
+while True:
+    try:
+        num = float(input("Enter a number: "))
+        if num in values:
+            print("Duplicate")
    #put rules and conditions
+        else:
+            values.append(num)
+            print("Unique")
    #determine if uniqie or duplicate
+    except ValueError:
+        print("Invalid input")
+        break
     #print output
-#dont accept invalid input
+    
