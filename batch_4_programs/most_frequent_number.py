7b5a09d2fa2000351e4f082ac41747a54e991f33
Carlos De Guzman <raremagic01@gmail.com>
2025-03-12 23:18:15 +0800

    while loop

--- batch_4_programs/most_frequent_number.py
+++ batch_4_programs/most_frequent_number.py
@@ -1,9 +1,16 @@
 #import counter
+from collections import Counter
 
 #define a function that takes a list of numbers as input
+def most_frequent_number():
+    numbers =[]
 
+    while True:
+        try:
+            numbers.append(int(input("enter a number")))
+        except ValueError:
+            break
     #construct while loop
 
 #set rules and conditions
-
 #execute command
