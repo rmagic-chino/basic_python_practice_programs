6a3b8e65600a9010f90cf713865de7b84d9f8c20
Carlos De Guzman <raremagic01@gmail.com>
2025-03-12 23:02:37 +0800

    print duplicates

--- batch_4_programs/display_duplicates.py
+++ batch_4_programs/display_duplicates.py
@@ -1,7 +1,10 @@
 #define command
-    #input: a list of strings
-    #output: a list of strings
+def find_duplicates():
+    numbers = [int(input("enter a number: ")) for i in range(10)]
+    duplicates = {num for num in numbers if numbers.count(num) > 1}
 
+    print("The duplicates are: ", duplicates)
     #print duplicate numbers
 
+find_duplicates()
 #execute command
