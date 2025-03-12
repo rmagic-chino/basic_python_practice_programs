0aaa54cfc66a40d17e185d12e447ece2b315264b
Carlos De Guzman <raremagic01@gmail.com>
2025-03-12 11:19:46 +0800

    from low to high numbers

--- batch_3_programs/low_to_high_numbers.py
+++ batch_3_programs/low_to_high_numbers.py
@@ -1,8 +1,17 @@
 #initialize
+numbers = []
 
 #while loop
     #input number
+while True:
+    try:
+        num = float(input("Enter a number: "))
+        numbers.append(num)
+    except ValueError:
+        break
     #set rules for the loop
 
 #initialize
+numbers.sort()
+print("numbers in order:", numbers)
 #print nums from low to high
