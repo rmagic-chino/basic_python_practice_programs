a9270c4958cad12a2f3c77a5c8042e419290d08b
Carlos De Guzman <raremagic01@gmail.com>
2025-03-13 09:20:20 +0800

    display highest number input

--- batch_4_programs/print_highest_num.py
+++ batch_4_programs/print_highest_num.py
@@ -1,8 +1,15 @@
 #initialize
+def highest_number():
+    numbers = []
 
     #set while loop
-       #input numbers
-
+    while True:
+        try:
+            numbers.append(int(input("enter a number: ")))
+        except ValueError:
+            break
 #print output
+    print("Highest number:", max(numbers) if numbers else "No numbers entered.")
 
 #execute 
+highest_number()
