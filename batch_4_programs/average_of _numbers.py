9c0830d7d1f76c18e0f0242288ec855df5f49d93
Carlos De Guzman <raremagic01@gmail.com>
2025-03-13 09:40:10 +0800

    print average of input numbers

--- batch_4_programs/average_of _numbers.py
+++ batch_4_programs/average_of _numbers.py
@@ -1,9 +1,21 @@
 #define commands
-    #initialize
+def average_number():
+    numbers = []
 
     #construct while loop
-       #input numbers
+    while True:
+        try:
+            numbers.append(int(input("enter a number: ")))
+        except ValueError:
+            break
 
     #set rules and conditions
+    if numbers:
+        avg = sum(numbers) / len(numbers)
+        print("average of numbers:")
+        print(avg)
+    else:
+        print("no numbers entered.")
 
-#execute 
+#execute
+average_number()
