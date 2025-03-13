7596a88db2e7614c3c001eedd2221308d8561b82
Carlos De Guzman <raremagic01@gmail.com>
2025-03-13 09:30:16 +0800

    print highest number to lowest

--- batch_4_programs/highest_to_lowest.py
+++ batch_4_programs/highest_to_lowest.py
@@ -1,9 +1,20 @@
 #define commands
-    #initialize
+def sort_numbers():
+    numbers = []
 
     #construct while loop
-       #input numbers
+    while True:
+        try:
+            numbers.append(int(input("enter a number")))
+        except ValueError:
+            break    
 
 #set rules and conditions
+    if numbers:
+        print("numbers from highest to lowest:")
+        print(sorted(numbers, reverse=True))
+    else:
+        print("no numbers entered")
 
 #execute
+sort_numbers()
